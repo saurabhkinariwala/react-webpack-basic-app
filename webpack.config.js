68b4affa5b4536bac6be76f287bfa6167427b68e
Saurabh Kinariwala <saurabhkinariwala@Saurabhs-MacBook-Air.local>
2018-04-11 00:54:58 +0530

    Commiting the react application with webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path"),
+
+    DIST_DIR = path.resolve(__dirname, "dist"), 
+    SRC_DIR = path.resolve(__dirname, "src"), 
+
+    config = {
+        entry: SRC_DIR + "/app/index.js", 
+        output: {
+            path: DIST_DIR + "/app",  
+            filename: "bundle.js",    
+            publicPath: "/app/"       
+        },
+        mode: "development",
+        module: {
+            rules:[
+                {
+                    test: /\.js?/,     
+                    include: SRC_DIR, 
+                    loader: "babel-loader", 
+                    query: {
+                        presets: ["react", "es2015", "stage-2"] 
+                    }
+                }
+            ]
+        }
+    }
+
+    module.exports = config;
